cbda417347cadd5a67aeb9a8e2e3f23a76625532
Rajarshi Ghosh <raj990319@gmail.com>
2026-03-31 13:39:27 +0530

    Add connection pooling to database engine

--- app/db/base.py
+++ app/db/base.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, create_engine
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=False)
+engine = create_engine(settings.DATABASE_URL,pool_pre_ping=True,pool_recycle=3600, echo=False,)
 
 def get_session():
     """
